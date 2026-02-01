60606039c045e14a8b857dd42277abcfc0436cb1
Soumya-RanjanBhoi <soumya6ranjan9@gmail.com>
2026-02-02 00:42:31 +0530

    exp completed

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='medical_chatbot',
+    version='0.0.0',
+    author='Soumya Ranjan Bhoi',
+    author_email='soumya.ranjan.bhoi1100@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
---
+++ template.sh
@@ -0,0 +1,17 @@
+mkdir -p src
+mkdir -p research
+mkdir -p src/pipeline
+
+touch src/__init__.py
+touch src/helper.py
+touch src/Models.py
+touch src/prompts.py
+touch src/pipeline/__init__.py
+touch src/pipeline/pipeline.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trail.ipynb
+touch requirements.txt
+
+echo "files created successfully"
